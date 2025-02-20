7175aabd7462b8bf15f799da10b416239da05623
Luke Yoo <w.lukeyoo@gmail.com>
2025-02-19 23:05:41 -0800

    Support touch press

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,18 @@ mod setup;
 mod systems;
 mod utils;
 
+fn has_user_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    touch_input: Res<Touches>,
+) -> bool {
+    keyboard_input.just_pressed(KeyCode::Space)
+        || keyboard_input.just_pressed(KeyCode::Enter)
+        || mouse_button_input.just_pressed(MouseButton::Left)
+        || mouse_button_input.just_pressed(MouseButton::Right)
+        || touch_input.any_just_pressed()
+}
+
 fn main() {
     App::new()
         .insert_resource(Score(0))
@@ -29,19 +41,19 @@ fn main() {
             Update,
             start_game
                 .run_if(in_state(GameState::Inactive))
-                .run_if(input_just_pressed(KeyCode::Space)),
+                .run_if(has_user_input),
         )
         .add_systems(
             Update,
             reset_game
                 .run_if(in_state(GameState::GameOver))
-                .run_if(input_just_pressed(KeyCode::Space)),
+                .run_if(has_user_input),
         )
         .add_systems(Update, gravity.run_if(in_state(GameState::Active)))
         .add_systems(
             Update,
             jump.run_if(in_state(GameState::Active))
-                .run_if(input_just_pressed(KeyCode::Space)),
+                .run_if(has_user_input),
         )
         .add_systems(Update, pipes.run_if(in_state(GameState::Active)))
         .add_systems(Update, update_score.run_if(in_state(GameState::Active)))
